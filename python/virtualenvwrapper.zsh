a7ec88fbab8d18fb81e1b8ac357317c27a0e23ba
Tamas Nepusz <ntamas@gmail.com>
2014-01-14 23:42:19 +0100

    removed virtualenvwrapper.zsh; using pyenv now

--- python/virtualenvwrapper.zsh
+++
@@ -1,7 +0,0 @@
-# Set up virtualenvwrapper
-VENV=`which virtualenv`
-if [ "x${VENV}" != x ]; then
-	export WORKON_HOME="${HOME}/virtualenvs"
-	source /usr/local/bin/virtualenvwrapper.sh
-	workon default
-fi
