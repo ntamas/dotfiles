f40fdd13c01a8f50f73d54cfab08a29627d4fdc6
Tamas Nepusz <ntamas@gmail.com>
2014-01-14 23:36:08 +0100

    removed rbenv path entry

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="$HOME/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="$HOME/bin:$HOME/.gem/ruby/2.0.0/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
