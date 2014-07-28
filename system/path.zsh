f7869411752a8ebc7c2b7d213d12f8e6a51b0140
Tamas Nepusz <ntamas@gmail.com>
2014-07-28 14:49:22 +0200

    minor path cleanup

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,2 @@
 export PATH="$HOME/bin:$HOME/.gem/ruby/2.0.0/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export MANPATH="/usr/local/man:$MANPATH"
