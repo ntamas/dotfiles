24d32552ce6564834a86ce7a56a1b9ee49a6c0b0
Tamas Nepusz <ntamas@gmail.com>
2014-10-18 22:42:09 +0200

    even more mutt tweaks

--- mail/mutt.symlink/urlview.sh
+++ mail/mutt.symlink/urlview.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 HAS_URLSCAN=1
-command -v urlscan >/dev/null 2>&1 || { HAS_URLSCAN=0 }
+command -v urlscan >/dev/null 2>&1 || { HAS_URLSCAN=0; }
 
 if [ $HAS_URLSCAN = 1 ]; then
+	export BROWSER=open
 	urlscan "$@"
 else
 	urlview "$@"
