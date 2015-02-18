410c7f71fee6a3bc91426a839a493788241203ab
Tamas Nepusz <ntamas@gmail.com>
2015-02-18 11:54:42 +0100

    better rbenv loading

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,17 +1,5 @@
-# rehash shims
-rbenv rehash 2>/dev/null
-
-# shell thing
-rbenv() {
-  command="$1"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
-
-  case "$command" in
-  shell)
-    eval `rbenv "sh-$command" "$@"`;;
-  *)
-    command rbenv "$command" "$@";;
-  esac
-}
+# Set up rbenv
+if [[ -d ~/.rbenv/shims ]]; then
+	export PATH="${HOME}/.rbenv/shims:$PATH"
+	eval "$(pyenv init -)"
+fi
