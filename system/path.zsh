d99a5c4eefe28d664197559beddc4d84602bcaf3
Tamas Nepusz <ntamas@gmail.com>
2015-10-25 16:16:35 +0100

    added npm

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="$HOME/bin:$HOME/.gem/ruby/2.0.0/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$HOME/bin:$HOME/.node/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:$MANPATH"
