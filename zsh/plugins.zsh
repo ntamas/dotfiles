8541a9b7c00b399c55361967d7ed90bf29031531
Tamas Nepusz <ntamas@gmail.com>
2019-05-09 14:09:50 +0200

    updated powerline prompt style

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,2 +1,8 @@
+typeset -A ZSH_HIGHLIGHT_STYLES
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
+ZSH_HIGHLIGHT_STYLES[alias]='fg=green,bold'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=green,bold'
+ZSH_HIGHLIGHT_STYLES[command]='fg=green,bold'
+
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
