16ab6839040150404195814cf126e14e0983f023
Tamas Nepusz <ntamas@gmail.com>
2021-11-16 11:13:53 +0100

    changed Android SDK path

--- android/env.zsh
+++ android/env.zsh
@@ -1,3 +1,2 @@
 # Set up the home directory of the Android SDK
-export ANDROID_HOME=/usr/local/share/android-sdk
-export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
+export ANDROID_SDK_ROOT=${HOME}/Library/Android/sdk
